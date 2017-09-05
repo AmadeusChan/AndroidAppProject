28dc2106ddd3799c84f6258f1408337bd0eaca9c
Amadeus Chan <greenclouds@foxmail.com>
2017-09-05 19:46:09 +0800

    Add NewsList

--- app/src/main/java/com/java/a31/androidappproject/models/NewsList.java
+++ app/src/main/java/com/java/a31/androidappproject/models/NewsList.java
@@ -4,11 +4,15 @@ import android.app.DownloadManager;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.java.a31.androidappproject.R;
 
 import org.json.JSONArray;
@@ -31,7 +35,11 @@ public class NewsList implements INewsList {
     private HashSet<String> showedNews=new HashSet<String>();
     private INewsFilter filter=null;
     private int currentPageNumber=1;
-    private Context context;
+    private Context context; // attention!!
+
+    public NewsList(Context context) {
+        this.context=context;
+    }
 
     private boolean getNetworkState() {
         ConnectivityManager cm=(ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -47,56 +55,65 @@ public class NewsList implements INewsList {
 
     @Override
     public void getMore(int size, int pageNo, final INewsListener<List<INewsIntroduction>> listener) {
-        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, R.string.get_latest_news_api+"?pageSize="+size+"?pageNo"+pageNo, null,
-                new Response.Listener<JSONObject>(){
+        String url="http://166.111.68.66:2042/news/action/query/latest"+"?pageNo="+pageNo+"&pageSize="+size;
+        Log.d("url", url);
+        //url="http://166.111.68.66:2042/news/action/query/latest?pageNo=1&pageSize=20";
+        StringRequest request=new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
                     @Override
-                    public void onResponse(JSONObject response) {
-
-                        // to parse JSONObject into list of news introductions
+                    public void onResponse(String response) {
+                        Log.d("response", response);
                         ArrayList<INewsIntroduction> list=new ArrayList<INewsIntroduction>();
+                        /*
+                        NewsIntroduction newsIntroduction=new NewsIntroduction();
+                        newsIntroduction.setTitle(response);
+                        list.add(newsIntroduction);
+                        */
                         try {
-                            JSONArray ja=response.getJSONArray("list");
-                            int len=ja.length();
-                            for (int i=0; i<len; ++i) {
-                                NewsIntroduction newsIntroduction=new NewsIntroduction();
+                            JSONObject reader=new JSONObject(response);
+                            JSONArray array=reader.getJSONArray("list");
+                            for (int i=0; i<array.length(); ++i) {
+                                JSONObject jo=array.getJSONObject(i);
                                 try {
-                                    JSONObject j=ja.getJSONObject(i);
-                                    newsIntroduction.setClassTag(j.getString("newsClassTag"));
-                                    newsIntroduction.setID("news_ID");
-                                    newsIntroduction.setSource("news_Source");
-                                    newsIntroduction.setTitle("news_Title");
-                                    newsIntroduction.setTime("news_Time");
-                                    newsIntroduction.setURL("news_URL");
-                                    newsIntroduction.setAuthor("news_Author");
-                                    newsIntroduction.setLanguage("lang_Type");
-                                    newsIntroduction.setVideos("news_Video");
-                                    newsIntroduction.setIntroduction("news_Intro");
-                                    // to be complete
-                                    newsIntroduction.setReadFlag(false);
-                                    newsIntroduction.setFavoriteFlag(false);
-                                    newsIntroduction.setImages(null);
-                                } catch(JSONException e) {
+                                    NewsIntroduction news=new NewsIntroduction();
+                                    news.setTitle(jo.getString("news_Title"));
+                                    news.setClassTag(jo.getString("newsClassTag"));
+                                    news.setID(jo.getString("news_ID"));
+                                    news.setSource(jo.getString("news_Source"));
+                                    news.setTime(jo.getString("news_Time"));
+                                    news.setURL(jo.getString("news_URL"));
+                                    news.setAuthor(jo.getString("news_Author"));
+                                    news.setLanguage(jo.getString("lang_Type"));
+                                    news.setVideos(jo.getString("news_Video"));
+                                    news.setIntroduction(jo.getString("news_Intro"));
+
+                                    news.setImages(null);
+                                    news.setFavoriteFlag(false);
+                                    news.setReadFlag(false);
+                                    list.add(news);
+                                } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
-                                list.add(newsIntroduction);
                             }
                         } catch (JSONException e) {
-                            list=null;
+                            e.printStackTrace();
                         }
 
-                        // to call the listener
                         listener.getResult(list);
-
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        listener.getResult(null);
+
                     }
                 }
         );
+        RequestQueue queue= Volley.newRequestQueue(context);
+        queue.add(request);
     }
+
+
 
     @Override
     public void setFilter(INewsFilter filter) {
